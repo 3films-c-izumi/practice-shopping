90ef49fe73bf4efe099b5d03f8e4ba21ccc93360
3films-c-izumi <c-izumi@3films.com>
2025-08-14 10:26:49 +0900

    usersテーブルにis_verifiedカラムとverification_tokenカラムを追加、Userモデルのcastsにis_verifiedを追加

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,19 @@
 
 @section('content')
 
-<h1>ログイン</h1>
-
+<div>
+    <h1>ログイン</h1>
+    <form method="POST">
+        <div>
+            <label for="login-email">メールアドレス</label>
+            <input type="text" name="email" id="login-email">
+        </div>
+        <div>
+            <label for="login-password">パスワード</label>
+            <input type="password" name="password" id="login-password">
+        </div>
+        <input type="submit" value="ログイン">
+    </form>
+</div>
 
 @endsection
